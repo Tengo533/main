b51de46e09b2d1c194270a248f1d0c9a8982fe35
Tengo533 <coolkalel123@gmail.com>
2024-03-08 16:42:45 +0000

    billionare program, added top ten and best country

--- College Work/Python/Term 2.5/PandasProcessingData/billionare.py
+++ College Work/Python/Term 2.5/PandasProcessingData/billionare.py
@@ -9,19 +9,38 @@ def richest_person():
     worth = df_rp["finalWorth"].to_string(index=False)
     print(f"The richest person in the world is {name} with a final worth of ${worth}!")
 
+def create_list(b):
+    c=[]
+    for a in b:
+        c.append(a)
+    return c
 
 def top_10():
-    print("tt")
+    df = pd.read_csv("Term 2.5\PandasProcessingData\Billionaires Statistics Dataset.csv")
+    t10 = df.head(11)
+    names = create_list(t10["personName"])
+    worths = create_list(t10["finalWorth"])
+    for x in range(1,11):
+        print(f"{x}. {names[x]} \n   Worth: {worths[x]}\n")
 
 def best_country():
-    print("bc")
+    df = pd.read_csv("Term 2.5\PandasProcessingData\Billionaires Statistics Dataset.csv")
+    country_finalvalue = {}
+
+    for country in df["country"].unique():
+        df_country = df.loc[df["country"] == country]
+        total_worth = df_country["finalWorth"].sum()
+        country_finalvalue[country] = total_worth
+
+    best_country = max(country_finalvalue, key=country_finalvalue.get)
+    print(f"The best country is {best_country} with a total wealth of {country_finalvalue[best_country]}")
 
 
 
 
 
 def menu():
-    print("#############\nMenu\n1. Richest Person 2. Top 10 3. Best Country")
+    print("#############\nMenu\n1. Richest Person \n2. Top 10 \n3. Best Country\n#############\n")
     while True:
         try:
             # Enter user choice
@@ -40,9 +59,8 @@ def menu():
             else:
                 print("Please input a correct option.")
 
-        except:
-            print("Please input a correct number.")
-
+        except ValueError as error:
+            print(f"ERROR: {error}")
 
 def main():
     menu()
